04353a364b4ec89fbef7e770314afbdbb865a22a
Madhusudan0906 <prajapatmadhusudan06@gmail.com>
2022-08-26 23:05:42 +0530

    home page almost done

---
+++ big-basket-clone/src/Components/YourDailyStaples.jsx
@@ -0,0 +1,47 @@
+import { Grid, GridItem,Heading,Center } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function getBO() {
+  return fetch("https://json-server-bb-clone.herokuapp.com/api/YourDailyStaples")
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(err);
+    });
+}
+export default function YourDailyStaples() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getBO()
+      .then((res) => {
+        setData([...res]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div style={{
+        width:"85%",
+        margin:"auto",
+        marginTop:"30px",
+        marginBottom:"30px",
+    }}>
+        <Center><Heading as='h3' size='lg'>
+            Your Dailt Staples
+  </Heading>
+  </Center>
+  <hr/>
+  <br/>
+      <Grid templateColumns="repeat(6, 1fr)" gap={6}>
+        {data.map((el) => {
+          return (
+            <GridItem w="100%" h="100%" bg="blue.500" key={el.id}>
+              <img src={el.image} alt="error" />
+            </GridItem>
+          );
+        })}
+      </Grid>
+    </div>
+  );
+}
