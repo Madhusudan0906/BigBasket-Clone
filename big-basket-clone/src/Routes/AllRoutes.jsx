d8b0ec7bff0969abfb3cba7fac181b171b751823
Madhusudan0906 <prajapatmadhusudan06@gmail.com>
2022-08-27 20:53:13 +0530

    some changes,login and cart

--- big-basket-clone/src/Routes/AllRoutes.jsx
+++ big-basket-clone/src/Routes/AllRoutes.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import {Routes,Route} from "react-router-dom";
 import Home from "./Home";
-import Login from "./Login";
 
 export default function AllRoutes(){
     return(<>
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
 
     </Routes>
     </>)
