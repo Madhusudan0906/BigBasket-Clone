ff1a7eb5f0fcd5d60032b25f546e6f439de84afc
Madhusudan Prajapat <103635175+Madhusudan0906@users.noreply.github.com>
2022-08-28 14:35:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import Navbar from "../Components/Navbar";
-import CarouselTop from "../Components/CarouselTop";
-import CarouselBottom from "../Components/CarouselBottom";
-import Footer from "../Components/Footer";
-import CategoryLinks from "../Components/CategoryLinks";
-import BankOffers from "../Components/BankOffers";
-import TopOffers from "../Components/TopOffers";
-import YourDailyStaples from "../Components/YourDailyStaples"
-import SnackStore from "../Components/SnackStore";
-import CleaningHousehold from "../Components/CleaningHousehold";
-import HomeKitchen from "../Components/HomeKitchen";
-import BrandStore from "../Components/BrandStore";
-import FruitVeg from "../Components/FruitVeg";
-import Beverages from "../Components/Beverages";
-import BeautyHygiene from "../Components/BeautyHygiene";
-import BestSellers from "../Components/BestSellers";
+import Navbar from "./Components/Navbar";
+import CarouselTop from "./Components/CarouselTop";
+import CarouselBottom from "./Components/CarouselBottom";
+import Footer from "./Components/Footer";
+import CategoryLinks from "./Components/CategoryLinks";
+import BankOffers from "./Components/BankOffers";
+import TopOffers from "./Components/TopOffers";
+import YourDailyStaples from "./Components/YourDailyStaples"
+import SnackStore from "./Components/SnackStore";
+import CleaningHousehold from "./Components/CleaningHousehold";
+import HomeKitchen from "./Components/HomeKitchen";
+import BrandStore from "./Components/BrandStore";
+import FruitVeg from "./Components/FruitVeg";
+import Beverages from "./Components/Beverages";
+import BeautyHygiene from "./Components/BeautyHygiene";
+import BestSellers from "./Components/BestSellers";
 import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
 import AllRoutes from "./Routes/AllRoutes"
@@ -21,7 +21,22 @@ import AllRoutes from "./Routes/AllRoutes"
 function App() {
   return (
     <ChakraProvider>
-    <AllRoutes />
+    <Navbar />
+    <CarouselTop />
+    <CategoryLinks />
+    <BestSellers />
+    <BankOffers />
+    <TopOffers />
+    <FruitVeg />
+    <YourDailyStaples />
+    <Beverages />
+    <SnackStore />
+    <CleaningHousehold />
+    <BeautyHygiene />
+    <HomeKitchen />
+    <CarouselBottom />
+    <BrandStore />
+    <Footer />
     </ChakraProvider>
   );
 }
